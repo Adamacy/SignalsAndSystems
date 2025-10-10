c742b699aa2d28368a310ebd542141ed3d5bb7f8
Adam Jakubiak <a.jakubiak.srem@gmail.com>
2025-10-10 15:43:26 +0200

    Finished part 3 task 2

---
+++ task2/part3.m
@@ -0,0 +1,22 @@
+close all;
+clear all;
+clc;
+
+v = load("u.dat");
+
+R = 0.01;
+
+p_inst = v.^2 / R;
+P_avg_total = mean(p_inst);
+
+disp("The average power of the signal is: " + P_avg_total + "W");
+
+num_samples = length(v);
+window_fraction = 0.10 * num_samples;
+
+k_round = round(0.10 * num_samples);
+p_avg_moving_round = movmean(p_inst, k_round);
+P_max = max(p_avg_moving_round);
+
+disp("The maximum power for 10% of the signal is: " + P_max + "W");
+
